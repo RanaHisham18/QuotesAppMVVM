bf2fbd634f0e3354b982c4afec0fd91e451847b9
Rana Hisham <hishamrana00@gmail.com>
2022-03-15 16:47:30 +0200

    Almost Done again

--- app/src/main/java/com/rana/mvvmrevisiontrial/ui/quotes/MainActivity.kt
+++ app/src/main/java/com/rana/mvvmrevisiontrial/ui/quotes/MainActivity.kt
@@ -2,6 +2,8 @@ package com.rana.mvvmrevisiontrial.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -23,13 +25,21 @@ class MainActivity : AppCompatActivity() {
         val viewmodel = ViewModelProviders.of(this,factory)
 
             .get(QuotesViewModel::class.java)
-        viewmodel.getQuote().observe(this, Observer { quotes ->  val StringBuilder = StringBuilder()
-        quotes.forEach{quote -> StringBuilder. append("$quote \n \n")}
+        viewmodel.getQuote().observe(this, Observer { quotes ->  val stringBuilder = StringBuilder()
+        quotes.forEach{quote -> stringBuilder. append("$quote \n \n")}
+ var TextViewQuote = findViewById<TextView>(R.id.textView_quote)
 
-authorTV= findViewById<TextView>(R.id.textView_quote)
+            TextViewQuote.text= stringBuilder.toString()
         })
-
-
+var AeditText = findViewById<EditText>(R.id.author_name)
+var QeditText = findViewById<EditText>(R.id.quoteET)
+var AddButton = findViewById<Button>(R.id.add_button)
+        AddButton.setOnClickListener{
+            val quote = Quote(QeditText.text.toString(),AeditText.text.toString())
+            viewmodel.addQuote(quote)
+            QeditText.setText("")
+            AeditText.setText("")
+                    }
     }
 
 }
